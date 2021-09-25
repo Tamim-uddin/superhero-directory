95597bb4a31c5b77f58f9c286b271d926f8e0600
Tamim-uddin <muhammadtamimuddin@gmail.com>
2021-09-26 01:32:17 +0600

    header added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Header.css'
 
 const Header = () => {
     return (
         <div className="header-container">
             <h1>List of the World's Billionaires</h1>
+            <p><small>The World's Billionaires is an annual ranking by documented net worth of the wealthiest billionaires in the world</small></p>
+            <h2>Total Worth: 900 Billion</h2>
         </div>
     );
 };
